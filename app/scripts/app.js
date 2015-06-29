413a89b16fb7304f684eed8f0cecfe2fe8b03ece
unknown <dakhtar@BP124.brightpower.local>
2015-06-29 17:01:32 -0400

    Completed Yeoman Angular App Exercise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,20 +15,25 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-    'ui.sortable'
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
   ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls'); 
+  }])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
   });
+  
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,37 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
   });
+
+  it('should add items to the list', function() {
+    scope.todo = 'Test 1'; 
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1); 
+  }); 
+
+  it('should add then remove an item from the list', function() {
+    scope.todo = 'Test 1';
+    scope.addTodo(); 
+    scope.removeTodo(0); 
+    expect(scope.todos.length).toBe(0); 
+  });
+
+//Mocking an HTTP request 
+
+//   it('should use a POST request when adding todos',
+//     inject(function(addTodo, $httpBackend) {
+
+//       $httpBackend.expect('POST', 'https://api.mydomain.com/login')
+//         .respond(200, "[{ success : 'true', id : 123 }]");
+
+//       LoginService.login('test@test.com', 'password')
+//         .then(function(data) {
+//           expect(data.success).toBeTruthy();
+//       });
+
+//     $httpBackend.flush();
+// });
+
 });
